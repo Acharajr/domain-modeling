ede30de91159bef2c3ba4d80dea09a746d1da209
Ugochukwu  Achara <Acharaachara2023@u.northwestern.edu>
2024-04-12 15:44:12 +0000

    created tables

--- school/school.sql
+++ school/school.sql
@@ -6,6 +6,8 @@ DROP TABLE IF EXISTS courses;
 DROP TABLE IF EXISTS sections;
 DROP TABLE IF EXISTS enrollments;
 
+-- Create the rest of the tables (code comment which is used to descripe what is going on and what you shpuld be doing within the code)
+
 CREATE TABLE students (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   first_name TEXT,
@@ -14,6 +16,24 @@ CREATE TABLE students (
   phone_number TEXT
 );
 
--- Create the rest of the tables
+CREATE TABLE teachers (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  bio TEXT
+);
 
+CREATE TABLE courses (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  description TEXT
+);
 
+CREATE TABLE sections (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  time TEXT,
+  course_id INTEGER,
+  teacher_id INTEGER
+);
+
+-- Do not add comma in the last line of the schema cause the program would think there is more coming. This will result in an error
