cf3f5fe92b4c8bbd5e5374414de1cb24c19aef79
Ugochukwu  Achara <Acharaachara2023@u.northwestern.edu>
2024-04-12 16:18:28 +0000

    class exercise 1

--- crm/crm.sql
+++ crm/crm.sql
@@ -3,3 +3,34 @@
 -- e.g. DROP TABLE IF EXISTS ______;
 
 -- CREATE TABLES
+CREATE TABLE contacts (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  email TEXT,
+  phone_number INTEGER,
+  company_id INTEGER
+);
+
+CREATE TABLE activities (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  type TEXT,
+  salesperson_id INTEGER,
+  contact_id INTEGER,
+  notes TEXT, 
+  date INTEGER
+);
+CREATE TABLE companies (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  industry TEXT
+);
+
+CREATE TABLE salespersons (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  phone_number INTEGER,
+  email TEXT,
+  contact_id INTEGER
+);
